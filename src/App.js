e67d9d22e60a35e72a94f541e65f3ffd040de358
Apoorv Pachori <apoorv.pachori21@gmail.com>
2021-12-06 18:10:51 -0800

    dependancy array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import TripList from "./components/TripList";
 
 function App() {
+
   return (
     <div className="App">
-    
+      <TripList> </TripList>
     </div>
   );
 }
